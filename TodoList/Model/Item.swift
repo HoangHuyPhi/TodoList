9987276b81bcfb9431578e006dc01436624515d5
Huy Hoàng <phihoanghuy@phis-macbook-pro.local>
2018-08-06 22:57:41 +1000

    Convert to MVC design pattern

---
+++ TodoList/Model/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  TodoList
+//
+//  Created by Phi Hoang Huy on 8/5/18.
+//  Copyright © 2018 Phi Hoang Huy. All rights reserved.
+//
+
+import Foundation
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
